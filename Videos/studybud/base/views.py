9e407aab0730085246df7b6a85ea0c273db1ecb7
madb3 <mjb926326@gmail.com>
2024-09-19 13:01:12 -0400

    test

--- Videos/studybud/base/views.py
+++ Videos/studybud/base/views.py
@@ -10,7 +10,12 @@ rooms = [
 
 def home(request):
     context = {'rooms':rooms}
-    return render(request, 'home.html', {'rooms':rooms})
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render('room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render('base/room.html', context)
